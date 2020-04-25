85486902806c4ff5b4351efd978684be0c9eaabc
yuhe <yuhe@erbu.com>
2020-04-25 15:39:48 +0800

    实现简单首页

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,12 @@
 // 首页路由
 import React, { Component } from 'react';
-
+import './home.less';
 export default class Home extends Component {
 
     render() {
         return(
-            <div>
-                Home
+            <div className='home'>
+                欢迎使用配置项测试管理系统
             </div>
         );
     }
