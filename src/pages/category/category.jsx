104d28a194acc0e0319b826f1df3077e582099ab
littlephoto <1605436974@qq.com>
2020-05-02 16:04:05 +0800

    完成一级列表与二级列表之间的跳转

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -3,20 +3,22 @@ import React, { Component } from 'react';
 import {
     Card,
     Table,
-    Pagination,
     Button,
     message
 } from 'antd';
-
-import { PlusCircleOutlined } from '@ant-design/icons';
+import { PlusCircleOutlined, ArrowRightOutlined } from '@ant-design/icons';
 import LinkButton from '../../components/link-button';
 import { reqCategories, reqAddCategory, reqUpdateCategory } from '../../api';
 
 export default class Category extends Component {
 
     state = {
-        categories: [],
-        loading: false
+        categories: [],  // 一级分类列表
+        subCategories:[], // 二级分类列表
+        loading: false,
+        parentId: null, // 当前需要显示的分类列表的父分类Id
+        parentName: '', // 当前需要显示的分类列表的父分类名称
+
     }
 
     // 初始化表格列
@@ -30,10 +32,12 @@ export default class Category extends Component {
             {
               title: '操作',
               width: '150px',
-              render:() => (//返回需要显示的界面标签
+              render:(category) => (//返回需要显示的界面标签
                 <span>
                     <LinkButton>修改</LinkButton>
-                    <LinkButton>子分类</LinkButton>
+                    {/* 如何向事件回调函数传递参数，先定义一个匿名函数，再函数中调用处理的函数，并传入数据 */}
+                    { this.state.parentId === null ? <LinkButton onClick={()=>{ this.showSubCategories(category) }}>子分类</LinkButton> : null }
+                    
                 </span>)
             },
           ];
@@ -45,11 +49,22 @@ export default class Category extends Component {
         // 在发请求前显示loading
         this.setState({ loading: true });
         // 发送请求
-        const response = await reqCategories();
+        const { parentId } = this.state;
+        const response = await reqCategories(parentId);
         const result = response.data;
         if(result.status === 'success'){
+            // 取出分类数组，可能是一级列表也可能是二级列表
             const { categories } = result;
-            this.setState({ categories });
+            if(parentId === null){
+                // 更新一级分类列表
+                this.setState({ categories });
+            } else {
+                // 更新二级分类列表
+                this.setState({
+                    subCategories: categories
+                });
+            }
+
         } else {
             message.error(result.message);
         }
@@ -57,6 +72,28 @@ export default class Category extends Component {
         // 结束loading
         this.setState({ loading: false });
         
+    }
+
+    //显示指定一级分类对象的二级列表
+    showSubCategories = (category) => {
+        // 更新状态，setState是异步更新的状态
+        this.setState({
+            parentId: category._id,
+            parentName: category.name
+        }, ()=>{
+            // 回调函数会在状态更新前且重新render后执行
+            this.getCategories();
+        });
+    }
+
+    // 显示一级分类列表
+    showCategories = () => {
+        this.setState({
+            // 更新为显示一级列表状态
+            parentId:null,
+            parentName: '',
+            subCategories:[]
+        });
     }
 
     // 发起异步请求，请求一级分类
@@ -70,10 +107,16 @@ export default class Category extends Component {
     }
     render() {
 
-        const { categories, loading } = this.state;
+        const { categories, subCategories, parentId, parentName, loading } = this.state;
 
         //卡片标题
-        const title = '一级分类列表';
+        const title = parentId === null ? '一级分类列表': (
+            <span>
+                <LinkButton onClick={ this.showCategories }>一级分类列表</LinkButton>
+                <ArrowRightOutlined style={{ marginRight:5}}/>
+                <span >{ parentName }</span>
+            </span>
+        )
         // Card的右侧
         const extra = (
             <Button type='primary'>
@@ -88,12 +131,12 @@ export default class Category extends Component {
                     bordered
                     loading={loading}
                     rowKey='_id'
-                    dataSource={categories}
+                    dataSource={ parentId==null ? categories : subCategories}
                     columns={this.columns}
                     pagination= {{
                         showSizeChanger: true,
                         showQuickJumper: true,
-                        defaultPageSize: 2,
+                        defaultPageSize: 5,
                         defaultCurrent: 1,
                         showTotal: num => `共有${num}项`,
                     }}
